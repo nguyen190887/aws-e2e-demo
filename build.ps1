33f74f3685d976cbe201753b4ef528e8174a016f
Tri Nguyen <nguyen190887@gmail.com>
2018-07-28 18:08:17 +0700

    Add s3 bucket and resize image function to tf

--- build.ps1
+++ build.ps1
@@ -3,4 +3,10 @@ cd $functionName
 dotnet publish --configuration Release
 Compress-Archive .\bin\Release\netcoreapp2.0\publish\* "..\$functionName.zip" -Force
 cd ..
+$functionName = "Lambda.ResizeImage"
+cd $functionName
+npm install
+Compress-Archive .\* "..\$functionName.zip" -Force
+cd ..
 terraform apply -auto-approve
+# terraform plan
--- deploy.tf
+++ deploy.tf
@@ -1,11 +1,18 @@
 provider "aws" {
-  region     = "us-west-2"
+  region = "us-west-2"
 }
 
-resource "aws_iam_role" "iam_CreateImageInfo" {
-  name = "Resource-Creation-Tagger-Role"
-	
-	assume_role_policy = <<EOF
+# Bucket: image
+resource "aws_s3_bucket" "ImageBucket" {
+  bucket = "rita167.e2edemo.gallery"
+  acl    = "private"
+}
+
+# Lambda iam role
+resource "aws_iam_role" "iam_Lambda" {
+  name = "e2edemo_LambdaRole"
+
+  assume_role_policy = <<EOF
 {
 	"Version": "2012-10-17",
 	"Statement": [
@@ -21,9 +28,10 @@ resource "aws_iam_role" "iam_CreateImageInfo" {
 EOF
 }
 
+# Lambda function: Create Image Info
 resource "aws_iam_role_policy" "iam_policy_CrateImageInfo" {
-  name            = "iam_policy_CrateImageInfo",
-  role = "${aws_iam_role.iam_CreateImageInfo.id}",
+  name = "iam_policy_CrateImageInfo"
+  role = "${aws_iam_role.iam_Lambda.id}"
 
   policy = <<EOF
 {
@@ -46,15 +54,70 @@ resource "aws_iam_role_policy" "iam_policy_CrateImageInfo" {
     ]
 }
 EOF
-
 }
 
 resource "aws_lambda_function" "CreateImageInfo" {
   filename         = "Lambda.CreateImageInfo.zip"
-  function_name    = "CreateImageInfo"
-  role             = "${aws_iam_role.iam_CreateImageInfo.arn}"
+  function_name    = "e2edemo_CreateImageInfo"
+  role             = "${aws_iam_role.iam_Lambda.arn}"
   handler          = "Lambda.CreateImageInfo::Lambda.CreateImageInfo.Function::FunctionHandler"
   source_code_hash = "${base64sha256(file("Lambda.CreateImageInfo.zip"))}"
   runtime          = "dotnetcore2.0"
   timeout          = "10"
+}
+
+# Lambda function: Resize Image
+resource "aws_iam_role_policy" "iam_policy_ResizeImage" {
+  name = "iam_policy_ResizeImage"
+  role = "${aws_iam_role.iam_Lambda.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "s3:*"
+            ],
+            "Effect": "Allow",
+            "Resource": "${aws_s3_bucket.ImageBucket.arn}/*"
+        },
+        {
+            "Action": [
+                "logs:*"
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "ResizeImage" {
+  filename         = "Lambda.ResizeImage.zip"
+  function_name    = "e2edemo_ResizeImage"
+  role             = "${aws_iam_role.iam_Lambda.arn}"
+  handler          = "index.handler"
+  source_code_hash = "${base64sha256(file("Lambda.ResizeImage.zip"))}"
+  runtime          = "nodejs8.10"
+  timeout          = "10"
+}
+
+resource "aws_lambda_permission" "AllowImageBucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.ResizeImage.arn}"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "${aws_s3_bucket.ImageBucket.arn}"
+}
+
+resource "aws_s3_bucket_notification" "ImageBucketNotification" {
+  bucket = "${aws_s3_bucket.ImageBucket.id}"
+
+  lambda_function = {
+    lambda_function_arn = "${aws_lambda_function.ResizeImage.arn}"
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "images/"
+  }
 }
